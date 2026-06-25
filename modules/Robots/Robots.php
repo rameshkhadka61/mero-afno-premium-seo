94870ea5175fa5125cab9ae6c740cbdcb1d1c4e4
Ramesh Khadka <rameshkhadka61@gmail.com>
2026-06-25 12:01:45 +0545

    feat: robust dynamic robots.txt generator

--- modules/Robots/Robots.php
+++ modules/Robots/Robots.php
@@ -5,15 +5,51 @@ namespace ESEO\Modules\Robots;
 class Robots {
 
     public function init() {
-        add_action( 'do_robotstxt', [ $this, 'custom_robots_txt' ], 10, 2 );
+        add_action( 'init', [ $this, 'register_rewrite_rules' ] );
+        add_filter( 'query_vars', [ $this, 'add_query_vars' ] );
+        add_action( 'template_redirect', [ $this, 'render_robots_txt' ] );
+        
+        // Keep the filter for default WP output just in case
+        add_filter( 'robots_txt', [ $this, 'custom_robots_txt' ], 10, 2 );
+    }
+
+    public function register_rewrite_rules() {
+        add_rewrite_rule( '^robots\.txt$', 'index.php?eseo_robots=1', 'top' );
+    }
+
+    public function add_query_vars( $vars ) {
+        $vars[] = 'eseo_robots';
+        return $vars;
+    }
+
+    public function render_robots_txt() {
+        if ( get_query_var( 'eseo_robots' ) ) {
+            header( 'Content-Type: text/plain; charset=utf-8' );
+            
+            $output = "User-agent: *\nDisallow: /wp-admin/\nAllow: /wp-admin/admin-ajax.php\n";
+            
+            $custom_rules = get_option( 'eseo_robots_txt', '' );
+            if ( ! empty( $custom_rules ) ) {
+                $output = $custom_rules . "\n";
+            }
+            
+            // Add sitemap
+            $sitemap_url = home_url( '/sitemap.xml' );
+            $output .= "\nSitemap: " . esc_url( $sitemap_url ) . "\n";
+            
+            // Allow other plugins to hook in if they use the standard WP hook
+            do_action( 'do_robotstxt' );
+            
+            echo apply_filters( 'eseo_robots_txt_output', $output );
+            exit;
+        }
     }
 
     public function custom_robots_txt( $output, $public ) {
-        // Here we could fetch custom rules from DB options
         $custom_rules = get_option( 'eseo_robots_txt', '' );
         
         if ( ! empty( $custom_rules ) ) {
-            return $custom_rules; // User has complete override
+            return $custom_rules;
         }
 
         return $output;
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: rameshkhadka
 Tags: seo
 Requires at least: 5.0
 Tested up to: 6.5
-Stable tag: 1.1.25
+Stable tag: 1.1.26
 Requires PHP: 7.4
 License: GPLv2 or later
 
@@ -13,5 +13,5 @@ An enterprise-level, highly optimized SEO plugin with integrated AI, advanced Sc
 An enterprise-level, highly optimized SEO plugin.
 
 == Changelog ==
-= 1.1.25 =
-* Removed metadata footprint.
+= 1.1.26 =
+* Added dynamic robots.txt generator.
